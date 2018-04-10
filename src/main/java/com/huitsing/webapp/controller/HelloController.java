816f564e26a64d73f7c547f4ea815d570061593b
GinYang <jin.yang@incurator.com>
2018-04-10 17:17:21 -0500

    commit1

--- src/main/java/com/huitsing/webapp/controller/HelloController.java
+++ src/main/java/com/huitsing/webapp/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.huitsing.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.huitsing.webapp.service.HelloService;
 
 
 @RestController
+@RequestMapping("/demo/")
 public class HelloController {
 	
 	@Autowired
@@ -32,5 +34,17 @@ public class HelloController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return new ResponseEntity<>(hello, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "hello", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> createHello(@RequestBody HelloModel helloModel) {
+		HelloModel hello = null;
+		try {
+			hello = helloService.getHello();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<>(true, HttpStatus.OK);
 	}
 }
