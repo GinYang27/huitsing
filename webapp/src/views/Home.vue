0fa7ab19727729dc4175d81df1c4d914c05de3b5
GinYang <jin.yang@incurator.com>
2018-05-17 17:11:02 -0500

    Merge HQ180502 into HQ1805

---
+++ webapp/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <ul>
+      <li>
+        <router-link to="/vue">Go Vue Default Page</router-link>
+      </li>
+      <li>
+        <router-link to="/page1">Go Page1</router-link>
+      </li>
+      <li>
+        <router-link to="/page2">Go Page2</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
