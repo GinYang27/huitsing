e35825e9704d37baa17959a1b2d657d60104f7d9
GinYang <jin.yang@incurator.com>
2018-05-21 16:15:03 -0500

    1st step

---
+++ webapp/src/components/HTTPDemo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    This is a HTTP Demo
+    <button @click="getData()">Click Me</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sampleData: ''
+      }
+    },
+    methods: {
+      getData() {
+        this.sampleData = 'This is some sample data';
+        console.log(this.sampleData);
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
