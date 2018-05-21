5de0b77e753870ed72cb4754a9f985572b60a151
GinYang <jin.yang@incurator.com>
2018-05-21 17:18:55 -0500

    cors config

--- webapp/src/components/HTTPDemo.vue
+++ webapp/src/components/HTTPDemo.vue
@@ -15,7 +15,12 @@
     methods: {
       getData() {
         this.sampleData = 'This is some sample data';
-        console.log(this.sampleData);
+        this.$http.get('http://localhost:8080/demo/hello/1').then(response => {
+          this.sampleData = response.data;
+          console.log(this.sampleData.title);
+        }, errResponse => {
+          console.error(errResponse)
+        })
       }
     }
   }
