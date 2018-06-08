a23d339ce2cd2c6028432273a2cf0e37c2353b82
GinYang <jin.yang@incurator.com>
2018-06-08 15:51:09 -0500

    home bg image

--- webapp/src/views/Overview.vue
+++ webapp/src/views/Overview.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Overview page here</div>
+<div class="h-ov-canvas">
+   <div class="container">
+    Overview page here
+    <h2></h2>
+  </div>
+</div>
+ 
 </template>
+
+<style lang="scss" scoped>
+  .h-ov-canvas {
+    background-image: url("../assets/hero.jpeg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    height: 100%;
+  }
+</style>
+
